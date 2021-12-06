c1f05108668d8d3620c8cc06a939f599f14bec12
Betsa Meneses <betsadmd@gmail.com>
2021-12-06 09:54:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const express= require('express');
+const mongoose = require('mongoose');
+const path= require('path');
+const userRoutes= require('./routes/user');
+const sauceRoutes= require('./routes/sauce');
+
+const app= express();// on appelle la methode express, ce qui permet de creer une aplication express//
+
+mongoose.connect('mongodb+srv://betsadmd:betsacl-149@cluster0.mqk3i.mongodb.net/Cluster0?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json()); // cette methode nous donne accès au corps de la requete//
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', sauceRoutes);
+
+module.exports = app; // on va exporter notre fichier pour pouvoir l'utiliser sur notre serveur//
---
+++ models/Sauce.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({ //La méthode  Schema  de Mongoose vous permet de créer un schéma de données pour votre bdd MongoDB.//
+  userId:{type: String, required: true},
+  name:{type: String, required: true},
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number},
+  dislikes: { type: Number },
+  usersLiked: { type: [String] },
+  usersDisliked: { type: [String] },
+});
+
+module.exports = mongoose.model('Sauce', sauceSchema); //La méthode  model  transforme ce modèle en un modèle utilisable.//
